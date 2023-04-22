bcc0d72e78430981d0266e860f7638c628bfce95
Geradot <toshkageradot@tuta.io>
2023-04-22 13:19:28 -0500

    Мелкие ошибки устранены

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -14,20 +14,23 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Script
+        <script
           src="https://kit.fontawesome.com/e4f737e708.js"
           crossOrigin="anonymous"
-        ></Script>
-        <Script
+          defer
+        ></script>
+        <script
           src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
           integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
           crossOrigin="anonymous"
-        ></Script>
-        <Script
+          defer
+        ></script>
+        <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
           integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
           crossOrigin="anonymous"
-        ></Script>
+          defer
+        ></script>
       </body>
     </Html>
   );
