2a6a787ccd4a65d3daeb8ed42ead3488529bab0d
Geradot <toshkageradot@tuta.io>
2023-04-22 14:46:47 -0500

    Вынес скрипты в head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -11,27 +11,27 @@ export default function Document() {
         integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ"
         crossOrigin="anonymous"
       />
+      <script
+        src="https://kit.fontawesome.com/e4f737e708.js"
+        crossOrigin="anonymous"
+        defer
+      ></script>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
+        crossOrigin="anonymous"
+        async
+      ></script>
       <body>
         <Main />
         <NextScript />
-        <script
-          src="https://kit.fontawesome.com/e4f737e708.js"
-          crossOrigin="anonymous"
-          defer
-        ></script>
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe"
-          crossOrigin="anonymous"
-          async
-        ></script>
 
         {/* <script
           src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js"
           integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE"
           crossOrigin="anonymous"
           async
-        ></script>
+        ></Main>
         <script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js"
           integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ"
